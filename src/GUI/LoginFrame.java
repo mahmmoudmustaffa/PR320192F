b497a5f114bede74f58f86baea75dbba15c67fbc
aashgar <aashgar@CASHGAR.iugaza.edu>
2019-03-03 13:45:11 +0200

    All Done ,,,

--- src/GUI/LoginFrame.java
+++ src/GUI/LoginFrame.java
@@ -4,12 +4,20 @@
  * and open the template in the editor.
  */
 package GUI;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
@@ -28,6 +36,8 @@ public class LoginFrame extends JFrame{
     private JPanel panelLogin;
     private JTextArea textArea;
     private JScrollPane scrollPane;
+    private JTextField textFieldData;
+    private JCheckBox checkBoxBold, checkBoxItalic;
 
     public LoginFrame(String title) {
         super(title);
@@ -43,9 +53,12 @@ public class LoginFrame extends JFrame{
         textFieldPassword = new JPasswordField(30);
         panelLogin.add(textFieldPassword);
         buttonOK=new JButton("OK");
-        buttonCancel=new JButton("CANCEL");
+        EventHandler eventHandler = new EventHandler();
+        buttonOK.addActionListener(eventHandler);
+        buttonCancel=new JButton("CANCEL");        
         panelLogin.add(buttonOK);
-        panelLogin.add(buttonCancel);        
+        panelLogin.add(buttonCancel); 
+        buttonCancel.addActionListener(eventHandler);
         textArea = new JTextArea();
         textArea.setText("Text Area Text Area \n" +
                 "Text Area Text Area \n"+
@@ -59,7 +72,42 @@ public class LoginFrame extends JFrame{
         setLayout(new FlowLayout(FlowLayout.CENTER));
         add(panelLogin);
         add(scrollPane);
+        textFieldData = new JTextField("Data", 40);
+        checkBoxBold = new JCheckBox("Bold");
+        checkBoxItalic = new JCheckBox("Italic");
+        EventHandler2 eventHandler2 = new EventHandler2();
+        checkBoxBold.addItemListener(eventHandler2);
+        checkBoxItalic.addItemListener(eventHandler2);
+        add(textFieldData);
+        add(checkBoxBold); add(checkBoxItalic);
+    }
+    private class EventHandler implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == buttonOK)
+            JOptionPane.showMessageDialog(null, 
+                    textFieldName.getText()+"\n" +
+                    String.valueOf(textFieldPassword.getPassword()));
+            else if(e.getSource() == buttonCancel)
+                System.exit(0);
+        }
         
+    }
+    private class EventHandler2 implements ItemListener{
+
+        @Override
+        public void itemStateChanged(ItemEvent e) {
+            if(e.getSource() == checkBoxBold)
+                textFieldData.setFont(
+                        new Font("Arial", Font.BOLD, 18));
+            else if(e.getSource() == checkBoxItalic)
+                textFieldData.setFont(
+                        new Font("Arial", Font.ITALIC, 18));
+            else
+                textFieldData.setFont(
+                        new Font("Arial", Font.PLAIN, 12));
+        }
         
     }
     

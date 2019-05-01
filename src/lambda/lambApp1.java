9903ca377772a4d552df37d594c3ec79dc3df733
aashgar <aashgar@CASHGAR.iugaza.edu>
2019-05-01 10:58:28 +0300

    All Done ,,,

---
+++ src/lambda/lambApp1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lambda;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author aashgar
+ */
+public class lambApp1 {
+    public static void main(String[] args) {
+        myOperation<Integer> add = (x,y) -> x * y;
+        System.out.println(add.operation(10, 5));
+        
+        Integer[] ar = {1, 2, 7, 3, 8, 4, 5, 6, 9, 10};
+        Arrays.stream(ar)
+                .sorted()
+                .forEach((x -> System.out.println(x)));
+
+        
+    }
+    
+    interface myOperation<T>{
+        T operation(T x, T y);
+    }
+}
